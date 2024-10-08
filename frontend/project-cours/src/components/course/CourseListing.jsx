16c6e919dc83270b2c64900942cdc47d02e75f69
Ashley R <ashleyrakotoarisoa@gmail.com>
2024-10-08 16:23:26 +0200

    Ajout du bouton "Ajouter un cours"

--- frontend/project-cours/src/components/course/CourseListing.jsx
+++ frontend/project-cours/src/components/course/CourseListing.jsx
@@ -6,9 +6,15 @@ const CourseListing = () => {
   return (
     <div className="bg-blue-100 h-screen w-screen flex items-center justify-center">
       <div className="bg-white w-3/4 h-2/3 rounded-lg p-8 overflow-auto flex flex-col">
-        <h1 className="text-4xl text-center font-bold text-blue-700 mx-auto">
-          Liste des cours :
-        </h1><br />
+        <div className="flex items-center justify-between mb-4 h-16">
+          <h1 className="text-4xl font-bold text-blue-700 mx-auto">
+            Liste des cours :
+          </h1>
+          <button className="text-xl bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400">
+            Ajouter un cours
+          </button>
+        </div>
+        <br />
         <table className="text-[22px] w-[85%] mx-auto border-collapse text-blue-700">
           <tr>
             <th className="border-2 border-grey w-[18%]">Nom du cours :</th>
@@ -34,7 +40,6 @@ const CourseListing = () => {
             <td id="tr2" class="p-[5px] cursor-pointer text-red-700 hover:text-red-500"><FontAwesomeIcon icon={faTrash} /></td>
           </tr>
         </table>
-        <button class="text-2xl mt-auto w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400">Retour</button>
       </div>
     </div>
   )
